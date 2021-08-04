34a409442bea282f71170935964899dfde54f20a
Arenarxi <86519884+Arenarxi@users.noreply.github.com>
2021-08-05 00:40:07 +0800

    Add files via upload

---
+++ minesweeper/src/settings.d6352.js
@@ -0,0 +1,1 @@
+window._CCSettings={platform:"web-desktop",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:false,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/scene/main.fire",orientation:"",jsList:[],bundleVers:{internal:"01588",main:"6bcb3"}};
